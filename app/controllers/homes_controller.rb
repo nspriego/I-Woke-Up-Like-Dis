1d9c8efce59c33d06d3005f274dcefdf304c983a
nspriego <nspriego@gmail.com>
2014-08-17 16:52:23 -0600

    Add Newsfeed. Redirect to newsfeed on sign in

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,14 @@
 class HomesController < ApplicationController
+	before_filter :check_user_logged_in
+	
 	def show
+	end
+
+	private
+
+	def check_user_logged_in
+		if signed_in?
+			redirect_to newsfeed_path
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#show', via: :get
+  resource :newsfeed, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 end
